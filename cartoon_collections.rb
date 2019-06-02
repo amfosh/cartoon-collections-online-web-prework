9e3d782619f8ebef4319ec2edcfc5c900bdcc817
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 22:16:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(words)
-  words.any? do |word|
-    word.length > 4
+  words.any? do { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
